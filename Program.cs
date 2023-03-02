9a977875d1358bc5429496a4ba713c0390dc8623
Polina Kolesnikova <polina@example.com>
2023-03-02 18:28:39 +0400

    all commenting

--- Program.cs
+++ Program.cs
@@ -21,26 +21,28 @@
 // string result = (firstNum==fifthNum && secondNum==forthNum)?("YES. Its palindrome"):("NO. It isnt palindrome!");
 // Console.WriteLine(result);
 
-Console.Write("Please, enter your 5-digit number:");
-int num = int.Parse(Console.ReadLine());
-int[] array = new int[5];
-int j = 10000;
-for (int i = 0; i < 5; i++)
-{
-    array[i] = num / j;
-    num = num%j;
-    j /= 10;
-}
-Console.WriteLine( $"{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}");
 
-if (array[0] == array[4] && array[1] == array[3])
-{
-    Console.WriteLine("YES, its palindrome!");
-}
-else
-{
-    Console.WriteLine("NO, it isnt palindrome");
-}
+//Second version with massives:
+// Console.Write("Please, enter your 5-digit number:");
+// int num = int.Parse(Console.ReadLine());
+// int[] array = new int[5];
+// int j = 10000;
+// for (int i = 0; i < 5; i++)
+// {
+//     array[i] = num / j;
+//     num = num%j;
+//     j /= 10;
+// }
+// Console.WriteLine( $"{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}");
+
+// if (array[0] == array[4] && array[1] == array[3])
+// {
+//     Console.WriteLine("YES, its palindrome!");
+// }
+// else
+// {
+//     Console.WriteLine("NO, it isnt palindrome");
+// }
 
 /*Задача 21
 
@@ -66,35 +68,35 @@ A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 // Second version:
 
-double[] parseCoordinates(string stringAnswer)
-{
-    double[] coordinates = new double[3]; 
-    string[] splitString = stringAnswer.Split(' ');
-    for(int i = 0; i < splitString.Length; i++)
-    {
-        coordinates[i] = double.Parse(splitString[i]);  
-    }
+// double[] parseCoordinates(string stringAnswer)
+// {
+//     double[] coordinates = new double[3]; 
+//     string[] splitString = stringAnswer.Split(' ');
+//     for(int i = 0; i < splitString.Length; i++)
+//     {
+//         coordinates[i] = double.Parse(splitString[i]);  
+//     }
 
-    return coordinates;
+//     return coordinates;
 
-}
-
-
-Console.Write("Please, enter first triplet numbers with space between:");
-string answerUser1 = Console.ReadLine();
-
-Console.Write("Please, enter second triplet numbers with space between:");
-string answerUser2 = Console.ReadLine();
+// }
 
 
-double result = 0;
+// Console.Write("Please, enter first triplet numbers with space between:");
+// string answerUser1 = Console.ReadLine();
 
-for (int i = 0; i < 3; i++)
-{
-    double part = Math.Pow(parseCoordinates(answerUser1)[i] - parseCoordinates(answerUser2)[i], 2);
-    result += part;
-}
-Console.WriteLine($"The distance is {Math.Round(Math.Sqrt(result), 2)}");
+// Console.Write("Please, enter second triplet numbers with space between:");
+// string answerUser2 = Console.ReadLine();
+
+
+// double result = 0;
+
+// for (int i = 0; i < 3; i++)
+// {
+//     double part = Math.Pow(parseCoordinates(answerUser1)[i] - parseCoordinates(answerUser2)[i], 2);
+//     result += part;
+// }
+// Console.WriteLine($"The distance is {Math.Round(Math.Sqrt(result), 2)}");
 
 /*Задача 23
 
