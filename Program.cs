d200be7179cddd0a5f67af10afb9c5fea18954d3
Polina Kolesnikova <polina@example.com>
2023-03-02 17:55:12 +0400

    adding ex 19

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@
 
 23432 -> да*/
 
+// Console.Write("Please, enter your 5-digit number:");
+// int num = int.Parse(Console.ReadLine());
+// int firstNum = num/10000;
+// int secondNum = (num%10000)/1000;
+// int thirdNum = (num%10000)%1000/100;
+// int forthNum = num%10000%1000%100/10;
+// int fifthNum = num%10;
+
+// Console.WriteLine($"{firstNum}, {secondNum}, {thirdNum}, {forthNum}, {fifthNum}");
+// string result = (firstNum==fifthNum && secondNum==forthNum)?("YES. Its palindrome"):("NO. It isnt palindrome!");
+// Console.WriteLine(result);
+
 
 
 /*Задача 21
@@ -18,20 +30,20 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
-Console.Write("Please, enter first triplet numbers with space between:");
-string[] splitString1 = Console.ReadLine().Split(' ');
-int x1 = int.Parse(splitString1[0]);
-int y1 = int.Parse(splitString1[1]);
-int z1 = int.Parse(splitString1[2]);
+// Console.Write("Please, enter first triplet numbers with space between:");
+// string[] splitString1 = Console.ReadLine().Split(' ');
+// int x1 = int.Parse(splitString1[0]);
+// int y1 = int.Parse(splitString1[1]);
+// int z1 = int.Parse(splitString1[2]);
 
-Console.Write("Please, enter second triplet numbers with space between:");
-string[] splitString2 = Console.ReadLine().Split(' ');
-int x2 = int.Parse(splitString2[0]);
-int y2 = int.Parse(splitString2[1]);
-int z2 = int.Parse(splitString2[2]);
+// Console.Write("Please, enter second triplet numbers with space between:");
+// string[] splitString2 = Console.ReadLine().Split(' ');
+// int x2 = int.Parse(splitString2[0]);
+// int y2 = int.Parse(splitString2[1]);
+// int z2 = int.Parse(splitString2[2]);
 
-double result = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)), 2);
-Console.WriteLine(result);
+// double result = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)), 2);
+// Console.WriteLine(result);
 
 /*Задача 23
 
