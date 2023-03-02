bcc20a537996475bd74304eb5d3995636d11624e
Polina Kolesnikova <polina@example.com>
2023-03-02 16:50:05 +0400

    adding ex 21

--- Program.cs
+++ Program.cs
@@ -18,9 +18,20 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
+Console.Write("Please, enter first triplet numbers with space between:");
+string[] splitString1 = Console.ReadLine().Split(' ');
+int x1 = int.Parse(splitString1[0]);
+int y1 = int.Parse(splitString1[1]);
+int z1 = int.Parse(splitString1[2]);
 
+Console.Write("Please, enter second triplet numbers with space between:");
+string[] splitString2 = Console.ReadLine().Split(' ');
+int x2 = int.Parse(splitString2[0]);
+int y2 = int.Parse(splitString2[1]);
+int z2 = int.Parse(splitString2[2]);
 
-
+double result = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)), 2);
+Console.WriteLine(result);
 
 /*Задача 23
 
@@ -30,10 +41,10 @@ A (7,-5, 0); B (1,-1,9) -> 11.53*/
 5 -> 1, 8, 27, 64, 125
 
 */
-Console.Write("Please, enter your number:");
-double num = double.Parse(Console.ReadLine());
-for ( double i = 1; i <= num; i++)
-{
-    double result = Math.Pow(i, 3);
-    Console.WriteLine($"Raising {i} to the third power is {result}");
-}
+// Console.Write("Please, enter your number:");
+// double num = double.Parse(Console.ReadLine());
+// for ( double i = 1; i <= num; i++)
+// {
+//     double result = Math.Pow(i, 3);
+//     Console.WriteLine($"Raising {i} to the third power is {result}");
+// }
